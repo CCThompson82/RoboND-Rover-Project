26cad9c6b83c713304faa5870bf0e5e2d86bf357
Chris Thompson <christophercthompson82@gmail.com>
2017-10-24 08:03:21 +0100

    Chore: Save progress playing with weighted average

--- code/decision.py
+++ code/decision.py
@@ -14,13 +14,54 @@ def decision_step(Rover):
     # Check if we have vision data to make decisions with
     if Rover.nav_angles is not None:
         print('MODE: {}'.format(Rover.mode))
+
+        obs_df = pd.DataFrame({'distance':Rover.obs_dists, 'angles':Rover.obs_angles})
+        right = -65
+        left = 20
+        max_threshold = 75 #NOTE: Lower equals more wall hugging
+        yaws = []
+        distances = []
+        yaws_df = pd.DataFrame([], columns = ['normal', 'tangent','distance','yaw',], index=[])
+        headings = range(right,left, 5)
+        for phi in headings:
+            phi = np.deg2rad(phi)
+
+            flt = np.random.random() / 1e4
+            phi_set =obs_df[(obs_df.angles > phi-0.05) & (obs_df.angles < phi+0.05)]
+            if len(phi_set) == 0 :
+                continue
+            else:
+                ar = np.min(phi_set.distance) - flt
+                a = np.min(phi_set.angles) - flt
+                br = np.max(phi_set.distance) + flt
+                b = np.max(phi_set.angles) + flt
+
+            if (ar > max_threshold) & (br > max_threshold) :
+                normal = 0
+                tangent = normal - (np.pi/2)
+            else :
+                ax, ay = np.sin(a)*ar, np.cos(a)*ar
+                bx, by = np.sin(b)*br, np.cos(b)*br
+
+                normal = ((bx - ax) / (ay - by)) + np.pi/2
+                tangent = normal - (np.pi/2)
+            yaw = normal - (np.pi/2)
+            yaws.append(yaw)
+            distances.append(np.mean([ar,br]))
+            yaws_df = yaws_df.append(pd.DataFrame({'normal':normal, 'tangent':tangent, 'yaw':yaw,
+                             'distance':np.min([ar,br])}, index = [phi]))
+        m1 = np.mean(Rover.nav_angles)
+        weights = 1 - (np.argsort(yaws_df.distance).argsort() / np.sum([x for x in range(len(yaws_df.distance))]))
+        m2 = np.average(yaws_df.yaw, weights = weights**2)
+        print(yaws_df)
+        m4 = np.mean([m1,m2])
+        m4 = m2
+
+
+        # Check the extent of navigable terrain
+
         # Check for Rover.mode status
         if Rover.mode == 'forward':
-            # Check the extent of navigable terrain
-            # if len(Rover.nav_angles) >= Rover.stop_forward:
-            #     # If mode is forward, navigable terrain looks good
-            #     # and velocity is below max, then throttle
-
             # TODO: logic for when to slow down
             if Rover.vel < Rover.max_vel:
                 # Set throttle value to throttle setting
@@ -28,62 +69,15 @@ def decision_step(Rover):
             else: # Else coast
                 Rover.throttle = 0
             Rover.brake = 0
-            # TODO: Add rover.steer logic
-            obs_df = pd.DataFrame({'distance':Rover.obs_dists, 'angles':Rover.obs_angles})
-            right = -50
-            left = 20
-            max_threshold = 100
-            yaws = []
-            distances = []
-            yaws_df = pd.DataFrame([], columns = ['normal', 'tangent','distance','yaw',], index=[])
-            headings = range(right,left, 5)
-            for phi in headings:
-                phi = np.deg2rad(phi)
-
-                flt = np.random.random() / 1e4
-                phi_set =obs_df[(obs_df.angles > phi-0.05) & (obs_df.angles < phi+0.05)]
-                if len(phi_set) == 0 :
-                    continue
-                else:
-                    ar = np.min(phi_set.distance) - flt
-                    a = np.min(phi_set.angles) - flt
-                    br = np.max(phi_set.distance) + flt
-                    b = np.max(phi_set.angles) + flt
-
-                if (ar > max_threshold) & (br > max_threshold) :
-                    normal = 0 + np.pi/2
-                    tangent = normal - (np.pi/2)
-                else :
-                    ax, ay = np.sin(a)*ar, np.cos(a)*ar
-                    bx, by = np.sin(b)*br, np.cos(b)*br
-
-                    normal = ((bx - ax) / (ay - by)) + np.pi/2
-                    tangent = normal - (np.pi/2)
-                yaw = normal - (np.pi/2)
-                yaws.append(yaw)
-                distances.append(np.mean([ar,br]))
-                yaws_df = yaws_df.append(pd.DataFrame({'normal':normal, 'tangent':tangent, 'yaw':yaw,
-                                 'distance':np.min([ar,br])}, index = [phi]))
-            print(yaws_df)
-            m1 = np.mean(Rover.nav_angles)
-            m2 = np.average(yaws_df.yaw, weights = (max_threshold - yaws_df.distance)/max_threshold)
-
-            m4 = np.mean([m1,m2])
-
             # Set steering to average angle clipped to the range +/- 15
             print('AVERAGE NAV YAW: {}'.format(np.median(Rover.nav_angles)))
             print('TARGET YAW: {}'.format(m4))
             Rover.steer = np.clip(m4 * 180/np.pi, -15, 15)
             print('ROVER STEERS: {}'.format(Rover.steer))
 
-            # # If there's a lack of navigable terrain pixels then go to 'stop' mode
-        elif len(Rover.nav_angles) < Rover.stop_forward:
-                # Set mode to "stop" and hit the brakes!
-                Rover.throttle = 0
-                # Set brake to stored brake value
-                Rover.brake = Rover.brake_set
-                Rover.steer = 0
+            if np.mean([np.median(yaws_df.distance), np.max(yaws_df.distance)]) < Rover.stop_forward:
                 Rover.mode = 'stop'
+
 
         # If we're already in "stop" mode then make different decisions
         elif Rover.mode == 'stop':
@@ -95,20 +89,20 @@ def decision_step(Rover):
             # If we're not moving (vel < 0.2) then do something else
             elif Rover.vel <= 0.2:
                 # Now we're stopped and we have vision data to see if there's a path forward
-                if len(Rover.nav_angles) < Rover.go_forward:
+                if np.mean([np.median(yaws_df.distance), np.max(yaws_df.distance)]) < Rover.go_forward:
                     Rover.throttle = 0
                     # Release the brake to allow turning
                     Rover.brake = 0
                     # Turn range is +/- 15 degrees, when stopped the next line will induce 4-wheel turning
-                    Rover.steer = -15 # Could be more clever here about which way to turn
+                    Rover.steer = 15 # Could be more clever here about which way to turn
                 # If we're stopped but see sufficient navigable terrain in front then go!
-                if len(Rover.nav_angles) >= Rover.go_forward:
+                if np.mean([np.median(yaws_df.distance), np.max(yaws_df.distance)]) >= Rover.go_forward:
                     # Set throttle back to stored value
                     Rover.throttle = Rover.throttle_set
                     # Release the brake
                     Rover.brake = 0
                     # Set steer to mean angle
-                    Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -25, 25)
+                    Rover.steer = np.clip(m4, -15, 15)
                     Rover.mode = 'forward'
     # Just to make the rover do something
     # even if no modifications have been made to the code
--- code/drive_rover.py
+++ code/drive_rover.py
@@ -55,15 +55,15 @@ class RoverState():
         self.rock_angles = None
         self.ground_truth = ground_truth_3d # Ground truth worldmap
         self.mode = 'forward' # Current mode (can be forward or stop)
-        self.throttle_set = 0.2 # Throttle setting when accelerating
+        self.throttle_set = 0.1 # Throttle setting when accelerating
         self.brake_set = 10 # Brake setting when braking
         # The stop_forward and go_forward fields below represent total count
         # of navigable terrain pixels.  This is a very crude form of knowing
         # when you can keep going and when you should stop.  Feel free to
         # get creative in adding new fields or modifying these!
-        self.stop_forward = 100 # Threshold to initiate stopping
-        self.go_forward = 101 # Threshold to go forward again
-        self.max_vel = 2 # Maximum velocity (meters/second)
+        self.stop_forward = 25 # Threshold to initiate stopping
+        self.go_forward = 30 # Threshold to go forward again
+        self.max_vel = 1 # Maximum velocity (meters/second)
         # Image output from perception step
         # Update this image to display your intermediate analysis steps
         # on screen in autonomous mode
